e34b244bf3f9770d291a3626d29b9bd03eb4d817
Sambit Mondal <sambitmondal2005@gmail.com>
2024-06-25 21:39:52 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const crime = document.querySelector('#crime');
 const year = document.querySelector('#year');
 
 function enableOthers(answer) {
-    if(answer.value != 0) {
+    if (answer.value != 0) {
         district.disabled = false;
         crime.disabled = false;
         year.disabled = false;
@@ -13,5 +13,19 @@ function enableOthers(answer) {
         district.disabled = true;
         crime.disabled = true;
         year.disabled = true;
+    }
+}
+
+function nextBtn() {
+    const stateOutput = state.value;
+    const districtOutput = district.value;
+    const crimeOutput = crime.value;
+    const yearOutput = year.value;
+
+    if ((stateOutput != 0) && (districtOutput != 0) && (crimeOutput != 0) && (yearOutput != 0)) {
+        window.location.href = '../html-files/graph-page.html';
+    }
+    else {
+        alert('Please select all Parameters!');
     }
 }
